82639bad47b064f5c32e7d887333befbbc21f33c
StarGazer1915 <justinklein0018@gmail.com>
2023-03-09 19:26:18 +0100

    Created function to update percepted terrain in attribute variable np array
    Created function to update percepted terrain in attribute variable, working on calculation of scores for this terrain next.

--- spawn_world.py
+++ spawn_world.py
@@ -13,6 +13,8 @@ import numpy as np
 class MarineBot(sc2.BotAI):
     def __init__(self):
         self.use_viz = False
+        self.vismap_stored = False
+        self.vismap_scores = np.array([])
         super().__init__()
 
     def on_start(self):
@@ -40,23 +42,27 @@ class MarineBot(sc2.BotAI):
 
         self.use_viz = True
 
-    def calc_scores(self):
-        # updated_map = self.state.visibility.data_numpy.copy()
-        # first_vision = self.state.visibility.data_numpy.copy()
-        # first_vision[first_vision == 2] = "X"
-        # self.visibility_scores = 0
+    def update_position_scores(self):
+        updated_map = self.state.visibility.data_numpy.copy()
+        print(f"updated map | type: {updated_map.dtype}, length: ({len(updated_map[0])}, {len(updated_map)})")
+        if not self.vismap_stored:
+            self.vismap_scores = updated_map.astype("int64")
+            self.vismap_stored = True
 
-        # for y in range(len(vgrid)):
-        #     for x in range(len(vgrid[0])):
-        #         if l_grid.is_set((x, y)):
-        #             np.put(l_grid[y], [x], [1])
-        #         else:
-        #             np.put(l_grid[y], [x], [0])
+        for y in range(len(updated_map)):
+            for x in range(len(updated_map[y])):
+                if updated_map[y][x] == 2:
+                    # CALCULATE SCORE AND REPLACE IN: self.vismap_scores
+                    self.vismap_scores[y][x] = 999
+                    pass
 
-        # for r in self.visibility_scores:
-        #     print(r)
+        print(f"vismap_scores | type: {self.vismap_scores.dtype}, "
+              f"length: ({len(self.vismap_scores[0])}, {len(self.vismap_scores)})")
 
-        # self.state.visibility.plot()
+        for r in self.vismap_scores:
+            print(r)
+
+        self.state.visibility.plot()
         return
 
     async def update_viewer(self):
@@ -91,7 +97,7 @@ class MarineBot(sc2.BotAI):
         await self.move_workers()
         await self.look_for_enemy()
 
-        self.calc_scores()
+        self.update_position_scores()
 
         # print(f"Unit Location: {self.workers[0].position}")
 
