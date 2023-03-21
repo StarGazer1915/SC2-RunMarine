2b4ddc6ed8384e35f8a5bf9ce5f911eb725e38f5
StarGazer1915 <justinklein0018@gmail.com>
2023-03-21 01:37:42 +0100

    Added docstring documentation, removed some lines of redundant code (class attributes)

--- src/GameBot.py
+++ src/GameBot.py
@@ -8,15 +8,16 @@ from MarineAgent import MarineAgent
 class GameBot(sc2.BotAI):
     def __init__(self):
         self.agents = []
-        self.vismap_stored = False
-        self.vismap_scores = np.array([])
-        self.valid_threshold = 0.8
         self.pathing_map = np.array([])
         self.map_y_size = 0
         self.map_x_size = 0
         super().__init__()
 
     def on_start(self):
+        """
+        Defines variables and attributes when the environment is initialized.
+        :return: void
+        """
         self.pathing_map = self.game_info.pathing_grid.data_numpy.astype("float64")
         self.map_y_size = len(self.pathing_map)
         self.map_x_size = len(self.pathing_map[0])
@@ -25,48 +26,66 @@ class GameBot(sc2.BotAI):
         return super().on_start()
 
     # ==================== MASKING FUNCTIONS ==================== #
-    def create_circular_mask(self, h, w, center=None, radius=None):
+    def create_circular_mask(self, center=None, radius=None):
+        """
+        This function creates a circular mask around a given index in a multidimensional array.
+        In this case it would be the StarCraft II map. It then returns this mask as a numpy array
+        with boolean values (True = Mask). This is used to (for example) create 'vision masks'
+        to determine the vision of a specific sc2 unit using the unit.sight_range attribute.
+        :param center: tuple
+        :param radius: int/float
+        :return: numpy array
+        """
         if center is None:  # use the middle
-            center = (int(w / 2), int(h / 2))
+            center = (int(self.map_x_size / 2), int(self.map_y_size / 2))
         if radius is None:  # use the smallest distance between the center and map edges
-            radius = min(center[0], center[1], w - center[0], h - center[1])
+            radius = min(center[0], center[1], self.map_x_size - center[0], self.map_y_size - center[1])
 
-        Y, X = np.ogrid[:h, :w]
+        Y, X = np.ogrid[:self.map_y_size, :self.map_x_size]
         dist_from_center = np.sqrt((X - center[0]) ** 2 + (Y - center[1]) ** 2)
-
-        mask = dist_from_center <= radius
-        return mask
+        return dist_from_center <= radius
 
     def create_baneling_masks(self, known_banelings):
+        """
+        This function creates different masks (numpy arrays) with different ranges for the banelings in
+        the current vision (known banelings) and stores them in a list which it then returns.
+        :param known_banelings: list of sc2 units
+        :return: list of masks
+        """
         mask_list = []
         for bane in known_banelings:
             pos = bane.position.rounded
             b_sight_range = bane.sight_range  # default 8.0
-            bmask1 = np.flip(self.create_circular_mask(
-                self.map_y_size, self.map_x_size, pos, b_sight_range-5.0), 0)
-            bmask2 = np.flip(self.create_circular_mask(
-                self.map_y_size, self.map_x_size, pos, b_sight_range-2.0), 0)
-            bmask3 = np.flip(self.create_circular_mask(
-                self.map_y_size, self.map_x_size, pos, b_sight_range), 0)
+            bmask1 = np.flip(self.create_circular_mask(pos, b_sight_range-5.0), 0)
+            bmask2 = np.flip(self.create_circular_mask(pos, b_sight_range-2.0), 0)
+            bmask3 = np.flip(self.create_circular_mask(pos, b_sight_range), 0)
             mask_list.append([bmask1, bmask2, bmask3])
 
         return mask_list
 
-    # ==================== ASYNC FUNCTIONS ==================== #
+    # ==================== STEP FUNCTION ==================== #
     async def on_step(self, iteration):
+        """
+        Function step by step:
+        1. Get the updated vision map and all known banelings that the bot sees on the map
+        2. For each agent (marine):
+            2.1 Create the agent's scoremask, make the agent percept the environment and list the banelings in vision.
+            2.2 If there is a baneling in the agent's current vision:
+                2.2.1 Apply the baneling 'Sphere Of Fear (sof)' for each baneling in known_banes (agent vision).
+                2.2.2 Make the agent think of, and move to, the next best possible position.
+
+        :param iteration: iteration (sc2)
+        """
         updated_map = self.state.visibility.data_numpy.astype("float64")
         baneling_list = [unit for unit in self.known_enemy_units if unit.name == "Baneling"]
-        for agent in self.agents:
-            score_mask = np.flip(self.create_circular_mask(self.map_y_size, self.map_x_size,
-                                                      agent.unit.position, agent.unit.sight_range), 0)
 
-            known_banes = [b for b in baneling_list if score_mask[b.position.rounded[1]][b.position.rounded[0]]]
+        for agent in self.agents:
+            score_mask = np.flip(self.create_circular_mask(agent.unit.position, agent.unit.sight_range), 0)
             agent.percept_environment(updated_map, score_mask)
+            known_banes = [b for b in baneling_list if score_mask[b.position.rounded[1]][b.position.rounded[0]]]
+
             if len(known_banes) > 0:
                 agent.apply_baneling_sof(score_mask, self.create_baneling_masks(known_banes))
                 time.sleep(0.05)
-                movement_mask = np.flip(self.create_circular_mask(
-                    self.map_y_size, self.map_x_size, agent.unit.position, agent.unit.sight_range - 2.5), 0)
+                movement_mask = np.flip(self.create_circular_mask(agent.unit.position, agent.unit.sight_range-2.5), 0)
                 await self.do(agent.take_action(movement_mask, known_banes))
-            else:
-                agent.known_banes = []
--- src/MarineAgent.py
+++ src/MarineAgent.py
@@ -4,8 +4,8 @@ from sc2.position import Point2
 
 class MarineAgent:
     def __init__(self, unit, passability_map, map_y_size, map_x_size):
-        self.unit = unit                            # sc2 unit object (Marine unit is this case)
-        self.vismap_stored = False                  # There is a vision map stored
+        self.unit = unit                            # sc2 unit object in-game (Marine unit is this case)
+        self.vismap_stored = False                  # If a vision map is stored
         self.vismap_scores = np.array([])           # The actual full vision map with its scored values
         self.valid_point_threshold = 0.8            # Threshold that determines if a point is valid to move towards
         self.passability_map = passability_map      # The passability map that shows what map points are passable
@@ -15,28 +15,33 @@ class MarineAgent:
 
     def pad_with(self, array, pad_width, iaxis, kwargs):
         """
-        Function that adds padding to an array to be able to create
-        scores for the outer edges when creating areas with create_area().
-        :param array: ndarray
-        :param pad_width: integer
-        :param iaxis: integer
-        :param kwargs: float
+        Takes an array and pads it (extends the outer edges of a grid).
+        :param array: numpy array / list
+        :param pad_width: int
+        :param iaxis: int
+        :param kwargs: kwarg object (int)
         """
         array[:pad_width[0]] = kwargs.get('padder', 10)
         array[-pad_width[1]:] = kwargs.get('padder', 10)
 
     def create_area(self, array, point, distance=2):
         """
-        Simple function that takes the values around a point (5x5 in this case)
-        and returns it as a np-array with the initial point in the center.
-        :param array: ndarray
+        Creates a new array/list in an area around a certain index (point). In the percept_environment() function
+        for example it will take values in a 5x5 area around the point, so the point is in the center. This way
+        a score can be generated on how passable a point is based on the area around the point.
+        :param array: numpy array / list
         :param point: tuple
-        :param distance: int (default 2)
-        :return: ndarray
+        :param distance: int
+        :return: numpy array / list
         """
         return array[point[0] - distance:point[0] + distance + 1, point[1] - distance:point[1] + distance + 1]
 
     def percept_environment(self, updated_map, vision_mask):
+        """
+
+        :param updated_map:
+        :param vision_mask:
+        """
         new_map = updated_map.copy()
         new_map[vision_mask != True] = 0.0
 
@@ -60,6 +65,15 @@ class MarineAgent:
                             self.vismap_scores[row][col] = 0.0
 
     def apply_baneling_sof(self, marine_mask, baneling_masks):
+        """
+        This function applies modifiers on the scoremap in the masks given by the baneling_masks.
+        These modifiers indicate to the agent how dangerous areas around the baneling are. These can be
+        seen as multiple circular layers and their impact becomes increasingly negative the closer the agent
+        moves towards the baneling. This way the fear of getting close to the baneling is simulated.
+        :param marine_mask: numpy array
+        :param baneling_masks: list of numpy arrays
+        :return: void
+        """
         for mask_set in baneling_masks:
             self.vismap_scores[(mask_set[2] == True) & (marine_mask == True)] *= 0.9
             self.vismap_scores[(mask_set[1] == True) & (marine_mask == True)] *= 0.6
@@ -67,9 +81,17 @@ class MarineAgent:
         self.vismap_scores = np.around(self.vismap_scores.copy(), 2)
 
     def define_state(self):
-        pass
+        return
 
     def take_action(self, vision_mask, known_banes):
+        """
+        This function looks at all the points inside the current vision of the agent and calculates the
+        highest scoring point that is also the farthest away from the baneling. It then returns the move
+        command with said point so the SC2bot can execute the move order.
+        :param vision_mask: numpy array
+        :param known_banes: list of sc2 unit objects
+        :return: sc2 move command
+        """
         highest_coor_in_vision = 0.0
         highest_scoring_coor = (0.0, 0.0)
         longest_distance_to_bane = 0.0
@@ -94,6 +116,3 @@ class MarineAgent:
 
     def __str__(self):
         return f"Agent: {self.unit.name}"
-
-
-
