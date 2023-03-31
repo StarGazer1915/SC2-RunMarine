15134a0398c7b29ab032f551dd9fff40fcf2e051
Youssef-Boulfiham <99402396+Youssef-Boulfiham@users.noreply.github.com>
2023-03-31 18:15:19 +0200

    (1): implement new map for enviroment testing., (2): adjust main.py in order to read map.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from src.GameBot import GameBot
 
-run_game(maps.get("marine_vs_baneling_advanced_NoOverlord"),
+run_game(maps.get("marine_vs_baneling"),
          [
              Bot(Race.Terran, GameBot()),
-             Computer(Race.Zerg, Difficulty.Hard)
+             Computer(Race.Zerg, Difficulty.VerryEasy)
          ], realtime=True)
 
 # Maps:
