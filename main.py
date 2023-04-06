ef2bc241a24da97eda29615393aa963cfedd43ff
UnknownZandbak <rjwillard01@gmail.com>
2023-04-06 18:02:55 +0200

    moved action matrix outside GameBot

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import numpy as np
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from src.GameBot import GameBot
 
+action_matrix = np.array([[[3, 3], [1, 4]], [[4, 1], [2, 2]]], dtype=np.float16)
 run_game(maps.get("marine_vs_baneling_advanced_NoOverlord_MultipleAgents"),
          [
-             Bot(Race.Terran, GameBot()),
+             Bot(Race.Terran, GameBot(action_matrix)),
              Computer(Race.Zerg, Difficulty.Hard)
          ], realtime=True)
 
