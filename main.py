b347eea9a1472e9a0d3a8f3e2d2546237463d64b
UnknownZandbak <rjwillard01@gmail.com>
2023-04-07 15:59:49 +0200

    init Action atrix outside of gamebot in main, Dict

--- main.py
+++ main.py
@@ -3,7 +3,31 @@ from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from src.GameBot import GameBot
 
-action_matrix = np.array([[[3, 3], [1, 4]], [[4, 1], [2, 2]]], dtype=np.float16)
+action_matrix = {
+    "Scores":{
+        "Attack": {
+            "Attack": (0., 0.),
+            "Flee": (0., 0.),
+        },
+        "Flee": {
+            "Attack": (0., 0.),
+            "Flee": (0., 0.),
+        },
+    },
+    # To keep track of the number of values have been added
+    # Helps with keeping a running average
+    "Counts":{
+        "Attack": {
+            "Attack": (0., 0.),
+            "Flee": (0., 0.),
+        },
+        "Flee": {
+            "Attack": (0., 0.),
+            "Flee": (0., 0.),
+        },
+    },
+}
+
 run_game(maps.get("marine_vs_baneling_advanced_NoOverlord_MultipleAgents"),
          [
              Bot(Race.Terran, GameBot(action_matrix)),
