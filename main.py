f35ee10c8dcd340e1e9c380ff78f28cf1c8fd520
Youssef-Boulfiham <99402396+Youssef-Boulfiham@users.noreply.github.com>
2023-04-11 18:40:50 +0200

    code clean; in main; comments and functiondecompensition

--- main.py
+++ main.py
@@ -5,24 +5,26 @@ from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from src.GameBot import GameBot
 
-for i in range(10):
+if __name__ == '__main__':
+    # epochs/generations
+    for i in range(10):
+        # check if action_matrix already exist
         try:
             with open("action_matrix.json", "r") as f:
                 action_matrix = json.load(f)
+        # otherwise create new action_matrix from template
         except:
             with open("action_matrix_template.json", "r") as f:
                 action_matrix = json.load(f)
 
         try:
-            run_game(maps.get("12SquareMarinevsBaneling"),
-                     [
-                         Bot(Race.Terran, GameBot(action_matrix, i)),
-                         Computer(Race.Zerg, Difficulty.Hard)
-                     ], realtime=True)
-        except Exception as err:
-            print(f"Error when runnign game: {err}")
-
-
+            # desired map
+            map = maps.get("12SquareMarinevsBaneling")
+            bot = [Bot(Race.Terran, GameBot(action_matrix, i)), Computer(Race.Zerg, Difficulty.Hard)]
+            # run
+            run_game(map, bot, realtime=True)
+        except Exception as error:
+            print(f"Error when running game: {error}")
 
 # ========== Maps ========== #
 # - marine_vs_baneling_advanced
@@ -31,4 +33,4 @@ for i in range(10):
 # - marine_vs_baneling_advanced_NoOverlord
 # - marine_vs_baneling_advanced_NoOverlord_noCliff
 # - marine_vs_baneling_advanced_NoOverlord_MultipleAgents
-
+# - marine_vs_baneling
